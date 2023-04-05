d350d581e2a036f397f6412efd40c98647575b52
phuong <phuongdenpro@gmail.com>
2023-04-05 14:25:33 +0700

    fix dto startDate endDate promotion

--- src/api/promotion/dto/update-promotion.dto.ts
+++ src/api/promotion/dto/update-promotion.dto.ts
@@ -25,17 +25,11 @@ export class UpdatePromotionDto {
 
   @ApiPropertyOptional({ example: '2023-03-01' })
   @IsDate({ message: 'START_DATE_IS_DATE' })
-  @MinDate(new Date(`${new Date().toDateString()}`), {
-    message: 'START_DATE_GREATER_THAN_NOW',
-  })
   @IsOptional()
   startDate: Date;
 
   @ApiPropertyOptional({ example: '2023-03-31' })
   @IsDate({ message: 'END_DATE_IS_DATE' })
-  @MinDate(new Date(`${new Date().toDateString()}`), {
-    message: 'END_DATE_GREATER_THAN_NOW',
-  })
   @IsOptional()
   endDate: Date;
 
