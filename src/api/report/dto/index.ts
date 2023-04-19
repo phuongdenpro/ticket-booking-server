2f7f8e13a098010199af03d6b71d82e7011ad1af
KayJuno <dangdan2807@gmail.com>
2023-04-20 00:15:10 +0700

    rename file ticket-Statistics.dto.ts to ticket-statistics.dto.ts

--- src/api/report/dto/index.ts
+++ src/api/report/dto/index.ts
@@ -1,4 +1,4 @@
-export * from './statistics.dto';
-export * from './top-customers-statistics.dto';
-export * from './ticket-statistics.dto';
 export * from './revenue-customers-statistics.dto';
+export * from './statistics.dto';
+export * from './ticket-statistics.dto';
+export * from './top-customers-statistics.dto';
